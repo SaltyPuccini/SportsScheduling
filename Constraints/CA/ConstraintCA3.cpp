2d2bb055f4a13497e0b1ef69034bbcb7ca2fdf48
Kajetan Wojciechowski <kkajetanwojciechowski@gmail.com>
2023-12-18 20:05:47 +0100

    Premiere implementation of CA3

--- Constraints/CA/ConstraintCA3.cpp
+++ Constraints/CA/ConstraintCA3.cpp
@@ -10,6 +10,72 @@ ConstraintCA3::~ConstraintCA3() {
 }
 
 bool ConstraintCA3::isViolated(Solution &solution) const {
+    std::vector<int> gameVector(mTeams1.size(), 0);
+    std::vector<int> lastPlayed(mTeams1.size(), -1);
+    for (int slot = 0; slot < solution.mSchedule.size(); slot++) {
+        switch (mMode1) {
+            case H:
+                for (int i = 0; i < solution.mSchedule[slot].size(); i++) {
+                    for (auto homeTeam: mTeams1) {
+                        for (auto enemyTeam: mTeams2) {
+                            if (solution.mSchedule[slot][i].isTrulyEqual({homeTeam, enemyTeam})) {
+                                lastPlayed[homeTeam] = slot;
+                                if (slot < lastPlayed[homeTeam] + mIntp){
+                                        gameVector[homeTeam] += 1;
+                                    }
+                            }
+                        }
+                    }
+                }
+                break;
+            case A:
+                for (int i = 0; i < solution.mSchedule[slot].size(); i++) {
+                    for (auto enemyTeam: mTeams1) {
+                        for (auto homeTeam: mTeams2) {
+                            if (solution.mSchedule[slot][i].isTrulyEqual({homeTeam, enemyTeam})) {
+                                lastPlayed[enemyTeam] = slot;
+                                if (slot < lastPlayed[enemyTeam] + mIntp){
+                                    gameVector[enemyTeam] += 1;
+                                }
+                            }
+                        }
+                    }
+                }
+                break;
+            case HA:
+                for (int i = 0; i < solution.mSchedule[slot].size(); i++) {
+                    for (auto homeTeam: mTeams1) {
+                        for (auto enemyTeam: mTeams2) {
+                            if (solution.mSchedule[slot][i].isPartiallyEqual({homeTeam, enemyTeam})) {
+                                lastPlayed[homeTeam] = slot;
+                                if (slot < lastPlayed[homeTeam] + mIntp){
+                                    gameVector[homeTeam] += 1;
+                                }
+                            }
+                        }
+                    }
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    int resultingPenalty = 0;
+
+    for (auto game : gameVector){
+        if (game > mMax) {
+            if (mType == SOFT) {
+                resultingPenalty += (game - mMax) * (mPenalty * mSoft);
+            } else {
+                resultingPenalty += (game - mMax) * (mPenalty * mHard);
+            }
+        }
+    }
+
+    if (resultingPenalty > 0) {
+        return true;
+    }
     return false;
 }
 
