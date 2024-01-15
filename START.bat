270b71c432c9e082ad761b49da2883d99c18cda4
Kajetan Wojciechowski <kkajetanwojciechowski@gmail.com>
2024-01-15 05:17:15 +0100

    Add configs and automation script

---
+++ START.bat
@@ -0,0 +1,27 @@
+::Late
+start /wait "Implementacja_AOIN_P" "Implementacja_AOIN_P.exe" Late4.xml SA0.config 5
+start /wait "Implementacja_AOIN_P" "Implementacja_AOIN_P.exe" Late4.xml SA1.config 5
+start /wait "Implementacja_AOIN_P" "Implementacja_AOIN_P.exe" Late4.xml SA2.config 5
+start /wait "Implementacja_AOIN_P" "Implementacja_AOIN_P.exe" Late4.xml SA3.config 5
+start /wait "Implementacja_AOIN_P" "Implementacja_AOIN_P.exe" Late4.xml SA4.config 5
+start /wait "Implementacja_AOIN_P" "Implementacja_AOIN_P.exe" Late4.xml SA5.config 5
+start /wait "Implementacja_AOIN_P" "Implementacja_AOIN_P.exe" Late4.xml SA6.config 5
+start /wait "Implementacja_AOIN_P" "Implementacja_AOIN_P.exe" Late4.xml SA7.config 5
+::Early
+start /wait "Implementacja_AOIN_P" "Implementacja_AOIN_P.exe" Early4.xml SA0.config 5
+start /wait "Implementacja_AOIN_P" "Implementacja_AOIN_P.exe" Early4.xml SA1.config 5
+start /wait "Implementacja_AOIN_P" "Implementacja_AOIN_P.exe" Early4.xml SA2.config 5
+start /wait "Implementacja_AOIN_P" "Implementacja_AOIN_P.exe" Early4.xml SA3.config 5
+start /wait "Implementacja_AOIN_P" "Implementacja_AOIN_P.exe" Early4.xml SA4.config 5
+start /wait "Implementacja_AOIN_P" "Implementacja_AOIN_P.exe" Early4.xml SA5.config 5
+start /wait "Implementacja_AOIN_P" "Implementacja_AOIN_P.exe" Early4.xml SA6.config 5
+start /wait "Implementacja_AOIN_P" "Implementacja_AOIN_P.exe" Early4.xml SA7.config 5
+::Middle
+start /wait "Implementacja_AOIN_P" "Implementacja_AOIN_P.exe" Middle4.xml SA0.config 5
+start /wait "Implementacja_AOIN_P" "Implementacja_AOIN_P.exe" Middle4.xml SA1.config 5
+start /wait "Implementacja_AOIN_P" "Implementacja_AOIN_P.exe" Middle4.xml SA2.config 5
+start /wait "Implementacja_AOIN_P" "Implementacja_AOIN_P.exe" Middle4.xml SA3.config 5
+start /wait "Implementacja_AOIN_P" "Implementacja_AOIN_P.exe" Middle4.xml SA4.config 5
+start /wait "Implementacja_AOIN_P" "Implementacja_AOIN_P.exe" Middle4.xml SA5.config 5
+start /wait "Implementacja_AOIN_P" "Implementacja_AOIN_P.exe" Middle4.xml SA6.config 5
+start /wait "Implementacja_AOIN_P" "Implementacja_AOIN_P.exe" Middle4.xml SA7.config 5
